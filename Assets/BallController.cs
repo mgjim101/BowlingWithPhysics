87544ff4e378604fd6cfb8301c9495ce81e87d50
Mohamed Gamal <mohamed.g.sakr@gmail.com>
2025-02-15 23:12:03 -0800

    Ball anchor created and some edits were done to ballcontroller. player and the cinemachine are now behind the ball

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,6 +5,7 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
 
     private Rigidbody ballRB;
     private bool isBallLaunched;
@@ -17,12 +18,17 @@ public class BallController : MonoBehaviour
         // Add a listener to the OnSpacePressed event.
         // When the space key is pressed, the LaunchBall method will be called.
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall()
     {
         if (isBallLaunched) return;
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
